c512d5c26565ef1f6449b63e770560710991c6ad
Boisselet Alexandre (IFAT DC ATV SC D TE2) <Alexandre.Boisselet@infineon.com>
2019-09-30 19:17:00 +0200

    add fig height slider

---
+++ .ipynb_checkpoints/00_MagpylibGui_voila-Copy1-checkpoint.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.2.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%capture --no-stdout
+# %run 01_Magpylib_Ipywidgets_Gui.ipynb
+
+display(app)
